22d8e6c9efcaa623ffb42d1aeef9948c7496d609
AVLAH ADAMA ELIYA <eliyaavlah@gmail.com>
2025-09-12 20:45:03 +0200

    Version du site DAAM-DEV

---
+++ script/script_job.js
@@ -0,0 +1,18 @@
+
+const liste_jobcard = document.getElementsByClassName("job-card");
+
+const modal1 = document.getElementById("modal1");
+const modal2 = document.getElementById("modal2");
+
+liste_jobcard[0].addEventListener("click",openModal1);
+liste_jobcard[1].addEventListener("click",openModal2);
+
+function openModal1() {
+    modal1.style.display = "flex"
+}
+function openModal2(){
+    modal2.style.display = "flex"
+}
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
