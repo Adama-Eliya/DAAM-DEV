1e0296a849a7e681d8e21a21196f4de87606248a
AVLAH ADAMA ELIYA <eliyaavlah@gmail.com>
2026-03-19 20:11:06 +0100

    mis a jour script_projet

--- script/script_projet.js
+++ script/script_projet.js
@@ -8,19 +8,25 @@ projets.forEach(element => {
     const div1 = document.createElement("div");
     const h3 = document.createElement("h3");
     const p = document.createElement("p");
-    const a = document.createElement("a");
+    const lienGit = document.createElement("a");
+    const lienDemo = document.createElement("a");
     div.appendChild(img);
     div.appendChild(div1);
     div1.appendChild(h3);
     div1.appendChild(p);
-    div1.appendChild(a);
+    div1.appendChild(lienGit);
     div.className = "card";
     div1.className = "card-content";
     img.src = element.img_src;
     img.alt = element.img_text;
     h3.textContent = element.title;
     p.textContent = element.description;
-    a.href = element.lien;
-    a.textContent = element.a_text;
+    lienGit.href = element.lien;
+    lienGit.textContent = element.a_text;
+    lienDemo.href = element.demo_lien
+    lienDemo.textContent = element.demo_text;
+    if (element.demo_lien) {
+        div1.appendChild(lienDemo);
+    }
     grid.appendChild(div);
 });
